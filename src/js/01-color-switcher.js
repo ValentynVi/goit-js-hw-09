be21099c45fcca06afe39746f455df7ef5f2001b
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-08-19 15:31:18 +0100

    add js code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const elements = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+let timerId = null;
+
+
+function intervalClickStart() {
+    elements.startBtn.disabled = true;
+    elements.stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+
+};
+
+
+
+function intervalClickStop() {
+    elements.startBtn.disabled = false;
+    elements.stopBtn.disabled = true;
+
+    clearInterval(timerId);
+};
+
+
+elements.startBtn.addEventListener('click', intervalClickStart);
+elements.stopBtn.addEventListener('click', intervalClickStop);
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
 
